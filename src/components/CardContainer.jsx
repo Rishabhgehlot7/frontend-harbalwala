70d8f8123246ac367fa60203f7855e93ba89a840
rishabh gehlot <rishabh3x@gmail.com>
2024-02-29 23:40:34 +0530

    cart commit

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -27,15 +27,15 @@ const CardContainer = () => {
                 <div>
                     {/* Your JSX */}
                     {isMobile ? (
-                        <section className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
+                        <section className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-5 gap-x-5 mt-10 mb-5">
                             {/* Product Cards */}
-                            {Array.from({ length: 6 }, (_, i) => (
+                            {Array.from({ length: 3 }, (_, i) => (
                                 <div key={i} className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-                                    <a href="#">
+                                    <a href="#" className=' flex'>
                                         <img
                                             src={`https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60${i}`}
                                             alt="Product"
-                                            className="h-80 w-72 object-cover rounded-t-xl"
+                                            className=" w-32 rounded-t-xl"
                                         />
                                         <div className="px-4 py-3 w-72">
                                             <span className="text-gray-400 mr-3 uppercase text-xs">Brand</span>
