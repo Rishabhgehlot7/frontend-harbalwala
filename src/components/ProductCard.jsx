7ae7eab15a04b76280001916e9686ade77d5d85c
rishabh gehlot <rishabh3x@gmail.com>
2024-03-02 18:00:50 +0530

    remove animation

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,33 +2,8 @@ import React, { useEffect } from 'react'
 import image01 from '../images/01.jpg'
 
 const ProductCard = () => {
-
-    useEffect(() => {
-        const callback = function (entries) {
-            entries.forEach((entry) => {
-                console.log(entry);
-
-                if (entry.isIntersecting) {
-                    entry.target.classList.add("animate-fadeIn");
-                } else {
-                    entry.target.classList.remove("animate-fadeIn");
-                }
-            });
-        };
-
-        const observer = new IntersectionObserver(callback);
-
-        const targets = document.querySelectorAll(".js-show-on-scroll");
-        targets.forEach(function (target) {
-            target.classList.add("opacity-0");
-            observer.observe(target);
-        });
-    }, [])
-
-
-
     return (
-        <div class="bg-gray-100 shadow-lg shadow-gray-500 rounded-lg p-4 flex flex-col items-center m-4 js-show-on-scroll">
+        <div class="bg-gray-100 shadow-lg shadow-gray-500 rounded-lg p-4 flex flex-col items-center m-4">
             <img src={image01} alt="" width={"100%"} className=' rounded-lg' />
             <h1 className=' p-2 font-medium text-xl'>Natural ToothPaste</h1>
             <div className=' p-2 text-xl'>
